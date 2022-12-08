dbdbe1cf09feb19a6abea135b7e67bebad61caf9
Carlos Catarino <carlos_cata27@hotmail.com>
2022-12-08 15:27:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const app = require("./app");
 const port = process.env.PORT || 3977;
 const cors = require("cors");
+const express = require("express");
+const logger = require("morgan");
+
 
 
 app.use(logger("dev"));
 
 app.disable("x-powered-by");
-
-
-app.listen(port, () => {
-  console.log(`Servidor funcionando en puerto ${port}`);
-});
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
@@ -27,15 +25,14 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(
   express.urlencoded({
-    extended: true,
-  })
+    extended: true,
+})
 );
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.status || 500).send(err.stack);
 });
 
-module.exports = {
-  app: app,
-  server: server,
-};
+app.listen(port, () => {
+  console.log(`Servidor funcionando en puerto ${port}`);
+});
